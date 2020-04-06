b00f68e5f32fbec2561272dbf585fbf16805a948
Berkay <berkay@solveatech.com>
2020-04-06 17:41:33 +0300

    Hand-made abstract class structure created

--- singleton-and-factory-pattern-example/src/Models/Shape.js
+++ singleton-and-factory-pattern-example/src/Models/Shape.js
@@ -1,11 +1,23 @@
+import React from "react";
 
 class Shape {
     x=0;
     y=0;
     color='white';
     constructor() {
+        if (new.target === Shape) {
+            throw new TypeError("Cannot construct Abstract instances directly");
+        }
+        if (this.getShape === undefined) {
+            throw new TypeError("Must override method getShape");
+        }
+        if (this.setShape === undefined) {
+            throw new TypeError("Must override method setShape");
+        }
+        if (this.draw === undefined) {
+            throw new TypeError("Must override method draw");
+        }
     }
-
 }
 
 export default Shape;
